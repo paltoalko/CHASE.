193188e0e62abe45c9bcf5e8fcf1979985b89f94
paltoalko <martas90909@me.com>
2022-10-22 22:13:26 +0100

    styling + assets

---
+++ src/assets/theme.ts
@@ -0,0 +1,28 @@
+import { createTheme, PaletteMode, responsiveFontSizes } from '@mui/material';
+
+let theme = createTheme({
+  palette: {
+    mode: 'light' as PaletteMode,
+    primary: {
+      main: '#EBE6E6',
+    },
+    secondary: {
+      main: '#52475A',
+    },
+    divider: '#EBE6E6',
+    text: {
+      primary: '#EBE6E6',
+      secondary: '#001220',
+    },
+    background: {
+      default: '#001220',
+    },
+  },
+  typography: {
+    fontFamily: 'Raleway',
+  },
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
