6376209d391906f074e698ae4055834bdefe696d
paltoalko <martas90909@me.com>
2022-11-06 11:34:48 +0000

    review box with switching

--- src/components/start/ReviewsBox.tsx
+++ src/components/start/ReviewsBox.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Typography, IconButton } from '@mui/material';
 import { ReactComponent as Wave } from '../../assets/svg/wave-review.svg';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
@@ -7,65 +7,108 @@ import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import styles from '../../assets/styles/ReviewsBox.module.css';
 
 interface BoxProps {
-  text: string;
+  index: number;
 }
 
-const reviews = [
-  {
-    text: `I've tried a lot of apps with a similar purpose but this is by far the best. Easy to use, pleasant UI/UX. Pro version definitely worth it.`,
-  },
-  {
-    text: `This app is great for all people. Finally one designed for programmers.`,
-  },
-  {
-    text: `You guys are the best! Now I can properly focus on my projects and finally I've finished many projects, that were too much time consuming for me.`,
-  },
-  {
-    text: `Very practical and usefull, I'm now using it everyday!`,
-  },
-  {
-    text: `Great app, I've even started hydrating myself properly because of the app notifs. `,
-  },
-];
-
-const Review: React.FC<BoxProps> = ({ text }) => {
+const Review: React.FC<BoxProps> = ({ index }) => {
+  const reviews = [
+    {
+      text: `I've tried a lot of apps with a similar purpose but this is by far the best. Easy to use, pleasant UI/UX. Pro version definitely worth it.`,
+    },
+    {
+      text: `This app is great for all people. Finally one designed for programmers.`,
+    },
+    {
+      text: `You guys are the best! Now I can properly focus on my projects and finally I've finished many projects, that were too much time consuming for me.`,
+    },
+    {
+      text: `Very practical and useful, I'm now using it everyday!`,
+    },
+    {
+      text: `Great app, I've even started hydrating myself properly because of the app notifs. `,
+    },
+    {
+      text: `Wow, imagine feeling good and not having your eyes tired after coding sesh. With that app it is possible! Def recommend.`,
+    },
+  ];
   return (
     <Typography
-      variant="body2"
+      variant="caption"
       fontWeight={200}
       color="primary"
       fontStyle="italic"
       className={styles.reviewText}
+      textAlign="center"
     >
-      {/* {text} */}
-      {reviews[0].text}
+      {reviews[index].text}
     </Typography>
   );
 };
 
 const ReviewBox: React.FC<{}> = () => {
-  //add swipe effect
+  const [index, setIndex] = useState(0);
+
+  const handleClick = (e) => {
+    if (e.target.id === 'left') {
+      if (index === 0) {
+        setIndex(5);
+      } else {
+        setIndex(index - 1);
+      }
+    } else if (e.target.id === 'right') {
+      if (index === 5) {
+        setIndex(0);
+      } else {
+        setIndex(index + 1);
+      }
+    }
+  };
 
   return (
     <Box className={styles.reviewsContainer}>
-      <Wave className={styles.svg} />
-      <Box className={styles.reviewsBox}>
-        <KeyboardArrowLeftIcon className={styles.icon} />
-        <Box className={styles.reviews}>
-          <Typography variant="h6" fontWeight={300} color="primary">
-            Your time matters:
-          </Typography>
-          <Review text="superb app" />
-          <Box className={styles.circleBox}>
-            <Box className={styles.circle} />
-            <Box className={styles.circle} />
-            <Box className={styles.circle} />
-            <Box className={styles.circle} />
-            <Box className={styles.circleActive} />
+      <Box className={styles.headerBox}>
+        <Typography
+          variant="h6"
+          fontWeight={300}
+          color="primary"
+          textAlign="center"
+        >
+          Your time matters:
+        </Typography>
+        <Box className={styles.reviewsBox}>
+          <IconButton size="small" onClick={(e) => handleClick(e)}>
+            <KeyboardArrowLeftIcon className={styles.icon} id="left" />
+          </IconButton>
+
+          <Box className={styles.reviews}>
+            <Review index={index} />
+            <Box className={styles.circleBox}>
+              <Box
+                className={index == 0 ? styles.circle : styles.circleActive}
+              />
+              <Box
+                className={index == 1 ? styles.circle : styles.circleActive}
+              />
+              <Box
+                className={index == 2 ? styles.circle : styles.circleActive}
+              />
+              <Box
+                className={index == 3 ? styles.circle : styles.circleActive}
+              />
+              <Box
+                className={index == 4 ? styles.circle : styles.circleActive}
+              />
+              <Box
+                className={index == 5 ? styles.circle : styles.circleActive}
+              />
+            </Box>
           </Box>
+          <IconButton size="small" onClick={(e) => handleClick(e)}>
+            <KeyboardArrowRightIcon className={styles.iconRight} id="right" />
+          </IconButton>
         </Box>
-        <KeyboardArrowRightIcon className={styles.icon} />
       </Box>
+      <Wave className={styles.svg} />
     </Box>
   );
 };
