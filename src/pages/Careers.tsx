b6d37e36558cbff72de33a8bc92968ee3a0b3f7d
paltoalko <martas90909@me.com>
2022-11-09 16:52:48 +0000

    created available careers card and centered it

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -4,11 +4,13 @@ import { Box, Button, Typography } from '@mui/material';
 import TopContainer from '../components/helpers/TopContainer';
 import { ReactComponent as CareersSvg } from '../assets/svg/careers.svg';
 import { ReactComponent as Waves } from '../assets/svg/waves-careers.svg';
+import { ReactComponent as WavesHeader } from '../assets/svg/waves-header.svg';
 import PerksContainer from '../components/careers/PerksContainer';
+import OpenPositions from '../components/careers/OpenPositions';
 
 const Careers: React.FC<{}> = ({}) => {
   return (
-    <Box className={styles.container}>
+    <Box>
       <TopContainer
         title="CAREERS"
         text="Are you ready to work with us?"
@@ -28,6 +30,16 @@ const Careers: React.FC<{}> = ({}) => {
         </Box>
       </Box>
       <PerksContainer />
+      <Typography
+        variant="h3"
+        fontWeight="700"
+        color="primary"
+        className={styles.subheaderWaveHeader}
+      >
+        Currently we are looking for
+      </Typography>
+
+      <OpenPositions />
     </Box>
   );
 };
