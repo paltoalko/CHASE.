696a3555e71f382b616b631fd22af1e9c58ff4a2
paltoalko <martas90909@me.com>
2022-11-15 14:42:41 +0000

    assets

---
+++ src/components/helpers/icons.ts
@@ -0,0 +1,47 @@
+import { ReactComponent as Classes } from '../../assets/svg/icons/classes.svg';
+import { ReactComponent as Cleaning } from '../../assets/svg/icons/cleaning.svg';
+import { ReactComponent as Drawing } from '../../assets/svg/icons/drawing.svg';
+import { ReactComponent as Designing } from '../../assets/svg/icons/designing.svg';
+import { ReactComponent as FinancialData } from '../../assets/svg/icons/financialdata.svg';
+import { ReactComponent as Interview } from '../../assets/svg/icons/interview.svg';
+import { ReactComponent as JobOffers } from '../../assets/svg/icons/joboffers.svg';
+import { ReactComponent as Meeting } from '../../assets/svg/icons/meeting.svg';
+import { ReactComponent as Mobile } from '../../assets/svg/icons/mobile.svg';
+import { ReactComponent as Podcast } from '../../assets/svg/icons/podcast.svg';
+import { ReactComponent as Programming } from '../../assets/svg/icons/programming.svg';
+import { ReactComponent as Projections } from '../../assets/svg/icons/projections.svg';
+import { ReactComponent as ReactSVG } from '../../assets/svg/icons/react.svg';
+import { ReactComponent as Reading } from '../../assets/svg/icons/reading.svg';
+import { ReactComponent as Running } from '../../assets/svg/icons/running.svg';
+import { ReactComponent as Studying } from '../../assets/svg/icons/studying.svg';
+import { ReactComponent as TeamWork } from '../../assets/svg/icons/team-work.svg';
+import { ReactComponent as Traveling } from '../../assets/svg/icons/traveling.svg';
+import { ReactComponent as Uiix } from '../../assets/svg/icons/uiix.svg';
+import { ReactComponent as Website } from '../../assets/svg/icons/website.svg';
+import { ReactComponent as WorkingOut } from '../../assets/svg/icons/working-out.svg';
+import { ReactComponent as Yoga } from '../../assets/svg/icons/yoga.svg';
+
+export const iconData = [
+  { id: 1, title: 'Classes', Image: Classes },
+  { id: 2, title: 'Cleaning', Image: Cleaning },
+  { id: 3, title: 'Designing', Image: Designing },
+  { id: 4, title: 'Drawing', Image: Drawing },
+  { id: 5, title: 'Financial Data', Image: FinancialData },
+  { id: 6, title: 'Interview', Image: Interview },
+  { id: 7, title: 'Job Offers', Image: JobOffers },
+  { id: 8, title: 'Meeting', Image: Meeting },
+  { id: 9, title: 'Mobile', Image: Mobile },
+  { id: 10, title: 'Podcast', Image: Podcast },
+  { id: 12, title: 'Programming', Image: Programming },
+  { id: 13, title: 'Projections', Image: Projections },
+  { id: 14, title: 'React', Image: ReactSVG },
+  { id: 15, title: 'Reading', Image: Reading },
+  { id: 16, title: 'Running', Image: Running },
+  { id: 17, title: 'Studying', Image: Studying },
+  { id: 18, title: 'Team Work', Image: TeamWork },
+  { id: 19, title: 'Traveling', Image: Traveling },
+  { id: 20, title: 'UI / UX', Image: Uiix },
+  { id: 21, title: 'Website', Image: Website },
+  { id: 22, title: 'Working Out', Image: WorkingOut },
+  { id: 23, title: 'Yoga', Image: Yoga },
+];
