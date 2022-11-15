cc00284c6f7cf9b101b02b900ebc1eedf1af8b07
paltoalko <martas90909@me.com>
2022-11-15 14:44:01 +0000

    marks constants added

---
+++ src/components/helpers/constants.ts
@@ -0,0 +1,16 @@
+export const marks = [
+  { value: 0, label: '0h' },
+  { value: 3, label: '3h' },
+  { value: 6, label: '6h' },
+  { value: 9, label: '9h' },
+  { value: 12, label: '12h' },
+  { value: 16, label: '16h' },
+  { value: 24, label: '24h' },
+];
+export const minuteMarks = [
+  { value: 5, label: '5m' },
+  { value: 15, label: '15m' },
+  { value: 30, label: '30m' },
+  { value: 45, label: '45m' },
+  { value: 55, label: '55m' },
+];
