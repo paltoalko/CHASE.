33c879bc129980f39d785e94b206e084a13614d0
paltoalko <martas90909@me.com>
2022-11-18 19:03:21 +0000

    calculate done time function added

--- src/components/helpers/timeCounter.ts
+++ src/components/helpers/timeCounter.ts
@@ -15,3 +15,32 @@ export const calculateTotalTime = (tasks) => {
 
   return time;
 };
+
+export const calculateDoneTime = (tasks) => {
+  let minutes;
+  let hours;
+  let time;
+
+  const taskHours = tasks.map((el) => el.hoursDone).reduce((a, b) => a + b, 0);
+  const taskMinutes = tasks
+    .map((el) => el.minutesDone)
+    .reduce((a, b) => a + b, 0);
+
+  if (taskHours == 0 && taskMinutes == 0) {
+    return (time = `0m`);
+  } else {
+    hours = Math.floor(taskMinutes / 60) + taskHours;
+
+    if (taskMinutes > 60) {
+      minutes = taskMinutes % 60;
+    } else {
+      minutes = taskMinutes;
+    }
+  }
+
+  time = `${hours}h ${minutes}m`;
+
+  return time;
+};
+
+// after every minu
